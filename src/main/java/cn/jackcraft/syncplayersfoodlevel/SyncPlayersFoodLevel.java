621acdd96505835292cda1d5e5d2ec6e639f9cab
Jack Lei <3486688394@qq.com>
2024-03-17 11:32:36 +0800

    feat: 实现同步饥饿值方法

--- src/main/java/cn/jackcraft/syncplayersfoodlevel/SyncPlayersFoodLevel.java
+++ src/main/java/cn/jackcraft/syncplayersfoodlevel/SyncPlayersFoodLevel.java
@@ -1,6 +1,12 @@
 package cn.jackcraft.syncplayersfoodlevel;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SyncPlayersFoodLevel extends JavaPlugin {
@@ -9,6 +15,14 @@ public final class SyncPlayersFoodLevel extends JavaPlugin {
     }
     @Override
     public void onDisable() {
-        HandlerList.unregisterAll(this);
+        HandlerList.unregisterAll((Plugin) this);
+    }
+
+    public static void syncFoodLevel(Player player, int foodLevel) {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            if (!p.equals(player) && p.isValid()) {
+                p.setFoodLevel(foodLevel);
+            }
+        }
     }
 }
