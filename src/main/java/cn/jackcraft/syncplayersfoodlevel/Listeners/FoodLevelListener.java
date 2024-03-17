0f6a1850f9118b613d3e1455757e5697173ee00f
Jack Lei <3486688394@qq.com>
2024-03-17 11:34:30 +0800

    feat: 添加饥饿值变化监听器

---
+++ src/main/java/cn/jackcraft/syncplayersfoodlevel/Listeners/FoodLevelListener.java
@@ -0,0 +1,17 @@
+package cn.jackcraft.syncplayersfoodlevel.Listeners;
+
+import cn.jackcraft.syncplayersfoodlevel.SyncPlayersFoodLevel;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+
+public class FoodLevelListener implements Listener {
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            SyncPlayersFoodLevel.syncFoodLevel(player, event.getFoodLevel());
+        }
+    }
+}
