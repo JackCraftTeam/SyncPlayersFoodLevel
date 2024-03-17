c2ad1888631f82af5d9ade99898675f079ae1c4a
Jack Lei <3486688394@qq.com>
2024-03-17 11:18:58 +0800

    feat: 禁用时取消注册

--- src/main/java/cn/jackcraft/syncplayersfoodlevel/SyncPlayersFoodLevel.java
+++ src/main/java/cn/jackcraft/syncplayersfoodlevel/SyncPlayersFoodLevel.java
@@ -1,5 +1,6 @@
 package cn.jackcraft.syncplayersfoodlevel;
 
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SyncPlayersFoodLevel extends JavaPlugin {
@@ -8,5 +9,6 @@ public final class SyncPlayersFoodLevel extends JavaPlugin {
     }
     @Override
     public void onDisable() {
+        HandlerList.unregisterAll(this);
     }
 }
