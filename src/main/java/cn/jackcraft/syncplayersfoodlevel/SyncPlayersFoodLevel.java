92b7a009b15f4b7a655fdb4df45db25034d1ae70
Jack Lei <3486688394@qq.com>
2024-03-17 11:15:06 +0800

    feat: 首次提交

---
+++ src/main/java/cn/jackcraft/syncplayersfoodlevel/SyncPlayersFoodLevel.java
@@ -0,0 +1,17 @@
+package cn.jackcraft.syncplayersfoodlevel;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SyncPlayersFoodLevel extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
