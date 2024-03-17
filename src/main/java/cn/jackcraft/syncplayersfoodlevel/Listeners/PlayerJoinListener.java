e3477ef295694b896ea9633b325678353121030a
Jack Lei <3486688394@qq.com>
2024-03-17 11:44:26 +0800

    feat: 加入后立即同步

---
+++ src/main/java/cn/jackcraft/syncplayersfoodlevel/Listeners/PlayerJoinListener.java
@@ -0,0 +1,36 @@
+package cn.jackcraft.syncplayersfoodlevel.Listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerJoinListener implements Listener {
+    @EventHandler(priority = EventPriority.LOW)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        Map<Integer, Integer> levelCounts = new HashMap<>();
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            int level = p.getInventory().getHeldItemSlot();
+            levelCounts.put(level, levelCounts.getOrDefault(level, 0) + 1);
+        }
+        int mostLevel = -1;
+        int maxLevel = 0;
+        for (Map.Entry<Integer, Integer> entry : levelCounts.entrySet()) {
+            int level = entry.getKey();
+            int count = entry.getValue();
+            if (count > maxLevel) {
+                mostLevel = level;
+                maxLevel = count;
+            }
+        }
+        if (mostLevel != -1) {
+            player.setFoodLevel(mostLevel);
+        }
+    }
+}
--- src/main/java/cn/jackcraft/syncplayersfoodlevel/SyncPlayersFoodLevel.java
+++ src/main/java/cn/jackcraft/syncplayersfoodlevel/SyncPlayersFoodLevel.java
@@ -1,19 +1,17 @@
 package cn.jackcraft.syncplayersfoodlevel;
 
 import cn.jackcraft.syncplayersfoodlevel.Listeners.FoodLevelListener;
+import cn.jackcraft.syncplayersfoodlevel.Listeners.PlayerJoinListener;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.FoodLevelChangeEvent;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SyncPlayersFoodLevel extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(new FoodLevelListener(), this);
+        Bukkit.getPluginManager().registerEvents(new PlayerJoinListener(), this);
     }
     @Override
     public void onDisable() {
