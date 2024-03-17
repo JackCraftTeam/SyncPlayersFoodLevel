c670b10cecd9df21cc5a37a57d08884b221bb26e
Jack Lei <3486688394@qq.com>
2024-03-17 11:45:43 +0800

    feat: 调整事件优先级

--- src/main/java/cn/jackcraft/syncplayersfoodlevel/Listeners/FoodLevelListener.java
+++ src/main/java/cn/jackcraft/syncplayersfoodlevel/Listeners/FoodLevelListener.java
@@ -3,11 +3,12 @@ package cn.jackcraft.syncplayersfoodlevel.Listeners;
 import cn.jackcraft.syncplayersfoodlevel.SyncPlayersFoodLevel;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 public class FoodLevelListener implements Listener {
-    @EventHandler
+    @EventHandler(priority = EventPriority.LOW)
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
         if (event.getEntity() instanceof Player) {
             Player player = (Player) event.getEntity();
