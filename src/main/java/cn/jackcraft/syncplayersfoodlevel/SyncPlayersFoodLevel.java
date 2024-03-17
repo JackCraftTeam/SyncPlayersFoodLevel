461985477e79b396b3a0dadaa29f74116f70acd5
Jack Lei <3486688394@qq.com>
2024-03-17 11:18:24 +0800

    refactor: 移除不必要注释

--- src/main/java/cn/jackcraft/syncplayersfoodlevel/SyncPlayersFoodLevel.java
+++ src/main/java/cn/jackcraft/syncplayersfoodlevel/SyncPlayersFoodLevel.java
@@ -3,15 +3,10 @@ package cn.jackcraft.syncplayersfoodlevel;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SyncPlayersFoodLevel extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
     }
-
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
